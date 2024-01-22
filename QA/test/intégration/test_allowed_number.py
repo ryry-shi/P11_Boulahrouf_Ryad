7230ce478d134c18eb7d5405eec577a79a761241
ryry-shi <ryadboulahrouf84@outlook.fr>
2024-01-22 15:31:27 +0100

    test_allowed_number

--- QA/test/intégration/test_allowed_number.py
+++ QA/test/intégration/test_allowed_number.py
@@ -18,6 +18,7 @@ def test_points_more_not_than_allowed(client, methods=["POST"]):
     )
     
     assert response.status_code == 200
-    assert "pas assez de points pour réservez de ticket" in response.get_data(
+    print(response.get_data(as_text=True))
+    assert "Vous n&#39;avez pas assez de points pour réservez ce montant de ticket" in response.get_data(
         as_text=True
     )
